d5fb50c939f33068ad4666d28fbd79b88475a0b7
Hugh Saunders <hugh@wherenow.org>
2019-03-09 11:59:23 +0000

    Tutorial creates LBs when requested
    The tutorial assumes that services created in K8S are accessible
    to the local machine. This may be the case with minikube and nodeport
    but is not so with a remotely hosted K8S.
    
    This commit adds functionality to the tutorial that allows a user
    to create LB services for the DB and quick start application so that
    they are accessible remotely.
    
    The environment variable CREATE_AND_CONFIGURE_LBS must be set to
    true to enable this functionality.

--- demos/k8s-demo/01_create_db.sh
+++ demos/k8s-demo/01_create_db.sh
@@ -31,6 +31,34 @@ kubectl --namespace quick-start-backend-ns \
 # Wait for it
 wait_for_app quick-start-backend quick-start-backend-ns
 
+# I considered making this a utils function, but transparent
+# commands are useful for a tutorial.
+if [[ "${CREATE_AND_CONFIGURE_LBS:-false}" == "true" ]]
+then
+  # Expose Service, useful if deploying to a remote K8s
+  kubectl expose svc quick-start-backend \
+      --type=LoadBalancer --port 5432 \
+      --target-port 5432 \
+      --name quick-start-backend-lb \
+      -n quick-start-backend-ns
+
+  echo ">>--- Waiting for external IP allocation"
+  # Wait for LB IP to be allocated
+  while kubectl get svc/quick-start-backend-lb\
+      -n quick-start-backend-ns |grep pending; do sleep 5; done
+  echo ">>--- External IP allocated"
+
+  # Get IP allocated to LB
+  DB_IP="$(kubectl get svc/quick-start-backend-lb \
+      -n quick-start-backend-ns \
+      -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
+
+  # Insert IP into admin config.
+  sed -i .bk \
+    's+^DB_URL=.*$+DB_URL="'${DB_IP}':5432/quick_start_db"+' \
+    admin_config.sh
+fi
+
 kubectl --namespace quick-start-backend-ns \
  exec \
  -i \
--- demos/k8s-demo/03_deploy_app.sh
+++ demos/k8s-demo/03_deploy_app.sh
@@ -17,4 +17,29 @@ kubectl --namespace quick-start-application-ns \
  apply \
  -f etc/quick-start-application.yml
 
+if [[ "${CREATE_AND_CONFIGURE_LBS:-false}" == "true" ]]
+then
+    # Expose Service, useful if deploying to a remote K8s
+    kubectl expose svc quick-start-application \
+        --type=LoadBalancer --port 8080 \
+        --target-port 8080 \
+        --name quick-start-application-lb \
+        -n quick-start-application-ns
+
+    echo ">>--- Waiting for external IP allocation"
+    while kubectl get svc/quick-start-application-lb\
+        -n quick-start-application-ns |grep pending; do sleep 5; done
+    echo ">>--- External IP allocated"
+
+    # Get IP allocated to LB
+    APP_IP="$(kubectl get svc/quick-start-application-lb \
+        -n quick-start-application-ns \
+        -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
+
+    # Insert IP into admin config.
+    sed -i .bk \
+        's/^APPLICATION_URL=.*$/APPLICATION_URL="'${APP_IP}':8080"/' \
+        admin_config.sh
+fi
+
 wait_for_app quick-start-application quick-start-application-ns
